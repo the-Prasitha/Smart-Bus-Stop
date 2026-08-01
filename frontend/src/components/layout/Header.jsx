a085f5b573f8da44711255eecf421f7839cfe0a6
the-Prasitha <prasitamil19@gmail.com>
2026-08-01 14:30:14 +0530

    Configure Tailwind CSS

---
+++ frontend/src/components/layout/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header className="bg-blue-700 text-white p-5 shadow-lg">
+      <h1 className="text-3xl font-bold">
+        🚍 Smart Bus Stop
+      </h1>
+
+      <p className="text-sm mt-2">
+        Real-Time Bus Tracking & Priority Seat Reservation
+      </p>
+    </header>
+  );
+}
+
+export default Header;
